bd98f5ccc721cabb03ffa49b9c05e9f58fff4933
Damjan Murn <damjan.murn@xlab.si>
2014-09-18 16:34:04 +0200

    Token signature is created from the header and payload json string instead from the compress and encoded value.

--- pki-tokens/pki-tokens-core/src/main/java/org/specs/pkitokens/core/Token.java
+++ pki-tokens/pki-tokens-core/src/main/java/org/specs/pkitokens/core/Token.java
@@ -11,12 +11,12 @@ import java.security.MessageDigest;
 import java.security.Signature;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Token {
-    private static final char SEPARATOR = '.';
+    private static final String SEPARATOR = ".";
 
     private Header header;
 
@@ -54,32 +54,39 @@ public class Token {
         header.setIssuer(signer.getSignerName());
         header.setSigningCertFingerprint(signer.getSignerFingerprint());
 
+        // convert to JSON
         String headerJson = JacksonSerializer.writeValueAsString(header);
         String payloadJson = JacksonSerializer.writeValueAsString(payload);
+
+        // compress the payload
         byte[] payloadCompressed = CompressUtils.compress(payloadJson);
 
+        // Base64 encode
         byte[] headerEncoded = Base64.encode(headerJson.getBytes());
-        byte[] claimsEncoded = Base64.encode(payloadCompressed);
+        byte[] payloadEncoded = Base64.encode(payloadCompressed);
 
-        byte[] separatorArray = String.valueOf(SEPARATOR).getBytes();
-        byte[] message = new byte[headerEncoded.length + claimsEncoded.length + 1];
-        System.arraycopy(headerEncoded, 0, message, 0, headerEncoded.length);
-        System.arraycopy(separatorArray, 0, message, headerEncoded.length, 1);
-        System.arraycopy(claimsEncoded, 0, message, headerEncoded.length + 1, claimsEncoded.length);
+        // prepare data to be signed
+        String dataToBeSigned = headerJson + payloadJson;
+        byte[] dataToBeSignedBytes = dataToBeSigned.getBytes("UTF-8");
 
+        // create the signature
         Signature sigInstance = Signature.getInstance("SHA256withRSA");
         sigInstance.initSign(signer.getSigningPrivateKey());
-        sigInstance.update(message);
+        sigInstance.update(dataToBeSignedBytes);
         byte[] signature = sigInstance.sign();
         byte[] signatureEncoded = Base64.encode(signature);
 
+        // compute the signature digest which is used as a token ID
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(signature);
         byte[] digest = md.digest();
         tokenId = DatatypeConverter.printHexBinary(digest);
 
-        StringBuilder sb = new StringBuilder(message.length + 1 + signatureEncoded.length);
-        sb.append(new String(message));
+        StringBuilder sb = new StringBuilder(headerEncoded.length + payloadEncoded.length +
+                signatureEncoded.length + 2);
+        sb.append(new String(headerEncoded));
+        sb.append(SEPARATOR);
+        sb.append(new String(payloadEncoded));
         sb.append(SEPARATOR);
         sb.append(new String(signatureEncoded));
         encodedValue = sb.toString();
@@ -89,46 +96,61 @@ public class Token {
 
     public static Token decode(String encodedToken, VerificationCertProvider verifCertProvider,
                                RevocationVerifier revocationVerifier) throws Exception {
-        int pos1 = encodedToken.indexOf(SEPARATOR);
-        int pos2 = encodedToken.indexOf(SEPARATOR, pos1 + 1);
-        byte[] encodedTokenBytes = encodedToken.getBytes();
 
-        String headerPart = encodedToken.substring(0, pos1);
-
-        String headerJson = new String(Base64.decode(headerPart.getBytes()));
-        Header header = JacksonSerializer.readValue(headerJson, Header.class);
-
-        // check expiry date
-        if (header.getExpiryDate().before(new Date())) {
-            throw new InvalidTokenException("The token is expired.");
+        String[] tokenParts = Pattern.compile(SEPARATOR, Pattern.LITERAL).split(encodedToken);
+        if (tokenParts.length != 3) {
+            throw new InvalidTokenException("Invalid format of the token.");
         }
 
+        // decode from Base64
+        byte[] headerBytes = Base64.decode(tokenParts[0].getBytes());
+        byte[] payloadBytes = Base64.decode(tokenParts[1].getBytes());
+        byte[] signatureBytes = Base64.decode(tokenParts[2].getBytes());
+
+        // deserialize the header
+        String headerJson = new String(headerBytes);
+        Header header = JacksonSerializer.readValue(headerJson, Header.class);
+
+        // check expiration date
+        if (header.getExpiryDate().before(new Date())) {
+            throw new InvalidTokenException("The token has expired.");
+        }
+
+        // decompress the payload
+        String payloadJson = CompressUtils.decompress(payloadBytes);
+
+        // get the signing certificate for signature verification
         X509Certificate verificationCert = verifCertProvider.getCertificate(header.getSigningCertFingerprint());
 
-        // verify signature
+        // prepare data for signature verification
+        String signedData = headerJson + payloadJson;
+
+        // verify the signature
         Signature sigInstance = Signature.getInstance(header.getSignatureAlgorithm());
         sigInstance.initVerify(verificationCert);
-        sigInstance.update(encodedTokenBytes, 0, pos2);
-        byte[] signature = Base64.decode(Arrays.copyOfRange(encodedTokenBytes, pos2 + 1, encodedTokenBytes.length));
-        boolean isVerified = sigInstance.verify(signature);
+        sigInstance.update(signedData.getBytes("UTF-8"));
+        boolean isVerified = sigInstance.verify(signatureBytes);
         if (!isVerified) {
             throw new InvalidTokenException("The token's digital signature is invalid.");
         }
 
+        // deserialize the payload
+        Payload payload = JacksonSerializer.readValue(payloadJson, Payload.class);
+
+        // compute the signature digest which is used as a token ID
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(signature);
+        md.update(signatureBytes);
         byte[] digest = md.digest();
         String tokenId = DatatypeConverter.printHexBinary(digest);
+
+        // check if the token has been revoked
         if (revocationVerifier != null) {
             if (revocationVerifier.isRevoked(tokenId)) {
                 throw new InvalidTokenException("The token has been revoked.");
             }
         }
 
-        byte[] claimsCompressed = Base64.decode(Arrays.copyOfRange(encodedTokenBytes, pos1 + 1, pos2));
-        String claimsJson = CompressUtils.decompress(claimsCompressed);
-        Payload payload = JacksonSerializer.readValue(claimsJson, Payload.class);
-
+        // create the token object
         Token token = new Token();
         token.header = header;
         token.payload = payload;
